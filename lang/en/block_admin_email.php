f3113d3bab2a31edd627d8020009e1a8b0256025
Robert Russo <rrusso@lsu.edu>
2013-07-30 09:22:39 -0500

    updatd lang to reference addinstance

--- lang/en/block_admin_email.php
+++ lang/en/block_admin_email.php
@@ -10,3 +10,4 @@ $string['body'] = 'Body';
 $string['email_error'] = 'Could not email: {$a->firstname} {$a->lastname} ({$a->email})';
 $string['email_error_field'] = 'Can not have an empty: {$a}';
 $string['block/admin_email:addinstance'] = "Add Admin Email block.";
+$string['admin_email:addinstance'] = "Add Admin Email block.";
